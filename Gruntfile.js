e4e61cde0e4bdfb358100bd35a9fae8fa5bc0165
mhawila <a.mhawila@gmail.com>
2015-07-31 15:17:05 +0300

    Changed grunt configuration to disable renaming of image files because usemin task is not replacing paths to these images, Also disabled cdnify

--- Gruntfile.js
+++ Gruntfile.js
@@ -205,7 +205,7 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+//          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
@@ -428,7 +428,7 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
+//    'cdnify',
     'cssmin',
     'uglify',
     'filerev',
